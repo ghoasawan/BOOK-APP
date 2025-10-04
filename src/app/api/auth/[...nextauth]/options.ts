5a38dc41885dd5f996355b1dc9e93ff49fd8779b
Ghoas Awan <ghoasawan12@gmail.com>
2025-10-05 04:20:43 +0500

    error resolves in authorize

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -16,7 +16,7 @@ export const authOptions: NextAuthOptions = {
         email: { label: 'Email', type: 'email' },
         password: { label: 'Password', type: 'password' }
       },
-      async authorize(credentials) {
+      async authorize(credentials) :Promise<any>{
         if (!credentials?.email || !credentials?.password) {
           throw new Error('Invalid credentials')
         }
