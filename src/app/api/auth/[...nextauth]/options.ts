8c4971bd1eed66dcfae0031cae4bd93247798244
Ghoas Awan <ghoasawan12@gmail.com>
2025-10-06 22:51:07 +0500

    fix: correct type casting for user ID in session callback

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -60,7 +60,7 @@ export const authOptions: NextAuthOptions = {
     },
     async session({ session, token }) {
       if (session.user) {
-        session.user.id = token.id as string
+        session.user.id = token.id as number;
       }
       return session
     }
